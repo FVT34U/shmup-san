463f0670acf694f802a63ef2cb4a21249ad24aab
FVT34U <captaintedotron55@gmail.com>
2026-05-13 17:24:22 +0400

    add assets and made parallax

---
+++ maps/world.gd
@@ -0,0 +1,4 @@
+extends Node2D
+class_name World
+
+@export var resolution: Vector2 = Vector2(360, 640)
--- player/player.gd
+++ player/player.gd
@@ -1,7 +1,10 @@
 extends CharacterBody2D
-
+class_name Player
 
 @export var speed = 200.0
+
+@onready var world: World = get_parent()
+@onready var sprite_size: Vector2 = %PlayerSprite.texture.get_size()
 
 
 func _physics_process(delta: float) -> void:
@@ -13,5 +16,5 @@ func _physics_process(delta: float) -> void:
 
 	move_and_slide()
 	
-	position.x = clampf(position.x, 0.0, 360.0)
-	position.y = clampf(position.y, 0.0, 640.0)
+	position.x = clampf(position.x, sprite_size.x, world.resolution.x - sprite_size.x)
+	position.y = clampf(position.y, sprite_size.y * 2, world.resolution.y - sprite_size.y)
