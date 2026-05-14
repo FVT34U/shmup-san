60b97c5ed503e97dcd0869a21306a5c808b65f75
FVT34U <captaintedotron55@gmail.com>
2026-05-14 17:37:41 +0400

    shoot projectiles from pool

--- player/player.gd
+++ player/player.gd
@@ -9,26 +9,38 @@ class_name Player
 @onready var sprite_size: Vector2 = %PlayerSprite.texture.get_size()
 @onready var pool: ObjectPool = %ProjectilePool
 @onready var shoot_point: Marker2D = %ShootPoint
+@onready var attack_timer: Timer = %AttackTimer
 
 var can_attack: bool = true
 
 
 func attack():
+	if not can_attack: return
+	
 	var projectile = pool.get_object_from_pool()
 	projectile.global_position = shoot_point.global_position
 	projectile.awake()
+	projectile.process_lifetime()
+	
 	return
 
 
 func disable_attack_for_time(disable_time: float):
-	pass
+	can_attack = false
+	await get_tree().create_timer(disable_time).timeout
+	can_attack = true
+	
+	return
 
 
 func _ready() -> void:
 	pool.init_pool(world)
-	await get_tree().create_timer(1.0).timeout
-	attack()
-	# set timer to call attack
+	
+	attack_timer.one_shot = false
+	attack_timer.autostart = false
+	
+	attack_timer.timeout.connect(attack)
+	attack_timer.start(attack_speed)
 
 
 func _physics_process(delta: float) -> void:
@@ -42,3 +54,6 @@ func _physics_process(delta: float) -> void:
 	
 	position.x = clampf(position.x, sprite_size.x, world.resolution.x - sprite_size.x)
 	position.y = clampf(position.y, sprite_size.y * 2, world.resolution.y - sprite_size.y)
+	
+	if Input.is_action_just_pressed("space"):
+		disable_attack_for_time(10.0)
--- systems/projectile_base.gd
+++ systems/projectile_base.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 class_name ProjectileBase
 
 @export var speed: float = 500.0
+@export var lifetime: float = 1.0
 
 var pool: ObjectPool = null
 var can_move: bool = false
@@ -10,18 +11,28 @@ var can_move: bool = false
 func init_to_pool(pool_ref: ObjectPool):
 	if !pool_ref: push_error("Pool is null")
 	pool = pool_ref
+	
 	return
 
 
 func awake():
 	can_move = true
-	await get_tree().create_timer(1.0).timeout
-	pool.return_object_to_pool(self)
+	visible = true
+	
 	return
 
 
 func sleep():
 	can_move = false
+	visible = false
+	
+	return
+
+
+func process_lifetime():
+	await get_tree().create_timer(lifetime).timeout
+	pool.return_object_to_pool(self)
+	
 	return
 
 
